c9fe5c6392063b28348d402929e2d85941de19d6
Jaimedev12 <89111266+Jaimedev12@users.noreply.github.com>
2025-04-28 18:04:24 -0600

    Entrega 1

--- BabyDuck/BabyTransformer.py
+++ BabyDuck/BabyTransformer.py
@@ -6,18 +6,10 @@ with open("grammar.lark") as f:
 
 parser = Lark(grammar, parser='lalr')
 
-
 # Define a Transformer to evaluate the parsed expression
 @v_args(inline=True)  # A decorator to simplify the method signatures
 class BabyTransformer(Transformer):
     def number(self, n):
         return float(n)
-
-    def add(self, a, b): return a + b
-    def sub(self, a, b): return a - b
-    def mul(self, a, b): return a * b
-    def div(self, a, b): return a / b
-    def neg(self, a): return -a
-    def product(self, a): return float(a)
 
 __all__ = ['parser', 'BabyTransformer']
